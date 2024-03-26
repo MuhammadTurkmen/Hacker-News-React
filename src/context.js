f55ce7790589d4610422221d7ae063a134869925
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-26 08:29:10 +0430

    init

--- src/context.js
+++ src/context.js
@@ -22,7 +22,7 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
   
   const fetchStories = async (url) => {
-    dispatch({type: 'SET_LOADING'})
+    dispatch({type: SET_LOADING})
   }
   
   useEffect(() => {
