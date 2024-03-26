1fd9cc7d0b5442ab3d904d0b18d128a9d8545347
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-26 17:25:14 +0430

    init

--- src/reducer.js
+++ src/reducer.js
@@ -11,6 +11,10 @@ const reducer = (state, action) => {
   switch(action.type) {
     case 'SET_LOADING': 
       return {...state, isLoading: true}
+    
+  default: 
+    throw new Error (`no matching "${action.type}" action type`)
+      
   }
   
 }
