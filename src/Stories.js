ea97a08aa2dcd12c499ee971a80e0f83f59e793e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-26 17:13:47 +0430

    init

--- src/Stories.js
+++ src/Stories.js
@@ -3,6 +3,13 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Stories = () => {
+  const {isLoading} = useGlobalContext()
+
+  
+  if(isLoading) {
+    
+  }
+  
   return <h2>stories component</h2>
 }
 
