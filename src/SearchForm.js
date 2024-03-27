6d5f2c951108af9cb2fb716ff80cde0fe422a750
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-27 09:58:56 +0430

    init

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -5,6 +5,7 @@ const SearchForm = () => {
   const {query, handleSearch} = useGlobalContext()
   return <form className='search-form' onSubmit={(e) => e.preventDefault()}>
     <h2>search hacker news</h2>
+    <input type='text' className='form-input'/>
   </form>
 }
 
