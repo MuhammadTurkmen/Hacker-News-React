461e616b09142d7b2faea4412c4dff12e52aba29
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-27 11:25:35 +0430

    init

--- src/Buttons.js
+++ src/Buttons.js
@@ -4,10 +4,13 @@ import { useGlobalContext } from './context'
 const Buttons = () => {
   const {isLoading, page, nbPages, handlePage} = useGlobalContext()
   return <div className='btn-container'>
-    <button disabled={isLoading} onClick={() => handlePage('decrease')}>
+    <button disabled={isLoading} onClick={() => handlePage('dec')}>
       prev
     </button>
     <p>some values</p>
+    <button disabled={isLoading} onClick={() => handlePage('inc')}>
+      prev
+    </button>
   </div>
 }
  
