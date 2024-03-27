a931204a46fe395b9cffc65a3d079cca202ab925
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-27 11:33:56 +0430

    init

--- src/context.js
+++ src/context.js
@@ -45,8 +45,7 @@ const AppProvider = ({ children }) => {
   }
 
   const handlePage = (value) => {
-    console.log(value)
-    // dispatch
+    dispatch({type: HANDLE_PAGE, payload: value})
   }
   
   
