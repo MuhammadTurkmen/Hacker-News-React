1bc38405d6dea7d7e65b0863e2a04f4eb2ba01c2
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-27 10:03:42 +0430

    init

--- src/reducer.js
+++ src/reducer.js
@@ -23,7 +23,7 @@ const reducer = (state, action) => {
       return {...state, hits: state.hits.filter((story) => story.objectID !== action.payload)}
 
     case HANDLE_SEARCH: 
-      return {...state, query: action.payload}
+      return {...state, query: action.payload, page: 0}
     default: 
       throw new Error (`no matching "${action.type}" action type`)
       
