fbc5ffaa111cb64d39594af305c2499e7351b2e8
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-27 09:55:26 +0430

    init

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const SearchForm = () => {
+  const {query, handleSearch} = useGlobalContext()
   return <h2>search form</h2>
 }
 
