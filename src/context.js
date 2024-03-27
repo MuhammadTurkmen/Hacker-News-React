42f1fa86b4b70774e7cafcf223dc60039a7e386c
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-27 11:49:50 +0430

    init

--- src/context.js
+++ src/context.js
@@ -51,7 +51,7 @@ const AppProvider = ({ children }) => {
   
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
-  }, [state.query])
+  }, [state.query, state.page])
 
   return <AppContext.Provider value={{...state, removeStory, handleSearch, handlePage}}>{children}</AppContext.Provider>
 }
