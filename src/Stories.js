42e495b4434e4e72ad8358a196d1456f1582157a
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-27 09:36:43 +0430

    init

--- src/Stories.js
+++ src/Stories.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Stories = () => {
-  const {isLoading, hits} = useGlobalContext()
+  const {isLoading, hits, removeStory} = useGlobalContext()
 
   
   if(isLoading) {
